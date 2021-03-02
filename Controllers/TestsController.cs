e8e6837955b3eaceae8da6c205978f682ffc3cac
igor <tk.igor@gmail.com>
2021-03-02 20:52:21 +0300

    Initial commit

---
+++ Controllers/TestsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WS_PerfTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestsController : ControllerBase
+    {
+        [HttpGet("get-dt")]
+        public string GetDt()
+        {
+            return DateTime.Now.ToString("O");
+        }
+    }
+}
